e5540a7c5578d0c26b1bcd9922533c5381bb41c2
SooKyungPark <waterglassclap@gmail.com>
2016-10-09 13:59:34 +0900

    implement tsp data handling codes

---
+++ christofides/tsp-data-handling/mst_builder.py
@@ -0,0 +1,84 @@
+""" build mst using scipy lib and tsp datas from : http://comopt.ifi.uni-heidelberg.de
+
+    References:
+    http://docs.scipy.org/doc/scipy-0.15.1/reference/generated/scipy.sparse.csgraph.minimum_spanning_tree.html
+    http://stackoverflow.com/questions/31139580/mst-clustering-using-python
+
+"""
+import os
+from tsp_fetcher import TSPFetcher
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import minimum_spanning_tree
+
+
+def makedir_safe(path):
+    """ if dir not exists, make dir. """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+class MSTBuilder(object):
+    """ mst builder for build mst using fully-connected graph infos """
+    def __init__(self):
+        self.outdir_root = os.path.abspath('datas')
+        self.outdir_tsp_full = os.path.join(self.outdir_root, 'tsp-full')
+        self.outdir_tsp_mst = os.path.join(self.outdir_root, 'tsp-mst')
+ 
+        makedir_safe(self.outdir_root)
+        makedir_safe(self.outdir_tsp_full)
+        makedir_safe(self.outdir_tsp_mst)
+
+
+    def build_all(self):
+        """ build all mst. """
+        tsp_configs = TSPFetcher().list()
+        for config in tsp_configs:
+            self.build(config)
+
+
+    def build(self, config):
+        """ build target mst from given config """
+        fully_matrix = self._get_fully_matrix(config)
+        self._build_mst(config, fully_matrix)
+
+
+    def _get_fully_matrix(self, config):
+        fully_file_name = os.path.join(self.outdir_tsp_full, config["name"] + ".txt")
+
+        with open(fully_file_name, "r") as infile:
+            line = infile.readline()
+            node_count, _ = tuple([int(x) for x in line.split(' ')])
+            fully_matrix = [[0 for i in range(node_count)] for j in range(node_count)]
+
+            while line:
+                line = infile.readline()
+                if line:
+                    start, end, weight = tuple([int(x) for x in line.split(' ')])
+                    fully_matrix[start][end] = weight
+
+        return fully_matrix
+
+
+    def _build_mst(self, config, fully_matrix):
+        mst_file_name = os.path.join(self.outdir_tsp_mst, config["name"] + ".txt")
+        with open(mst_file_name, "w") as outfile:
+            # header : node_count edge_count
+            outfile.write("%d %d" % (config["node_count"], config["node_count"] - 1))
+            # build mst
+            tcsr_arr = minimum_spanning_tree(csr_matrix(fully_matrix)).toarray().astype(int)
+            # contents : start_node end_node weight
+            for row_idx, row in enumerate(tcsr_arr):
+                for col_idx, item in enumerate(row):
+                    if item:
+                        outfile.write("\n%d %d %d" % (row_idx, col_idx, item))
+
+
+def sample():
+    """ sample for MSTBuilder """
+    tsp_configs = TSPFetcher().list()
+    builder = MSTBuilder()
+    builder.build(tsp_configs[0])
+    #builder.build_all()
+
+
+if __name__ == "__main__":
+    sample()
---
+++ christofides/tsp-data-handling/tsp_fetcher.py
@@ -0,0 +1,100 @@
+""" fetch tsp datas from : http://comopt.ifi.uni-heidelberg.de """
+from StringIO import StringIO
+from xml.etree import ElementTree
+import re
+import os
+import json
+import zipfile
+import requests
+import pycurl
+
+def simply_curl(url, buf):
+    """ simply curl from given url. """
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEDATA, buf)
+    c.perform()
+    c.close()
+
+
+def makedir_safe(path):
+    """ if dir not exists, make dir. """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+class TSPFetcher(object):
+    """ tsp fetcher which fetch tsp datas from : http://comopt.ifi.uni-heidelberg.de """
+    def __init__(self):
+        self.root_url = "http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95"
+        self.list_url = self.root_url + "/STSP.html"
+        self.tsp_url = self.root_url + "/XML-TSPLIB/instances/%s.xml.zip"      
+
+        self.list_item_pattern = re.compile(r"<big>([a-zA-Z\-\_]*)([0-9]*) : (\d+)</big></li>")
+
+        self.outdir_root = os.path.abspath('datas')
+        self.outdir_raws = os.path.join(self.outdir_root, 'raws')
+        self.outdir_tsp_full = os.path.join(self.outdir_root, 'tsp-full')
+ 
+        makedir_safe(self.outdir_root)
+        makedir_safe(self.outdir_raws)
+        makedir_safe(self.outdir_tsp_full)
+
+
+    def list(self, **kwargs):
+        """ list all stsp configs which contains name and optimal cost. """
+        buf = StringIO()
+        simply_curl(self.list_url, buf)
+        raw_lists = self.list_item_pattern.findall(buf.getvalue())
+        configs = [{"name" : item[0] + item[1], "node_count" : int(item[1]), "optimal" : item[2]}
+                   for item in raw_lists]
+
+        if "file" in kwargs and kwargs["file"]:
+            outfile_path = os.path.join(self.outdir_root, "list.json")
+            with open(outfile_path, "w") as outfile:
+                outfile.write(json.dumps(configs))
+        else:
+            return configs
+
+
+    def fetch_all(self):
+        """ fetch all stsp datas. """
+        configs = self.list()
+        for config in configs:
+            self.fetch(config)
+
+
+    def fetch(self, config):
+        """ fetch target stsp datas from given config """
+        # Download xml data file from remote url
+        req = requests.get(self.tsp_url % config["name"], stream=True)
+        zpfile = zipfile.ZipFile(StringIO(req.content))
+        zpfile.extractall(self.outdir_raws)
+
+        # parse xml file and save it as blossomV input form, which has integer weight
+        rawfile_name = os.path.join(self.outdir_raws, config["name"] + ".xml")
+        doc = ElementTree.parse(rawfile_name)
+        graph = doc.getroot().find("graph")
+
+        outfile_path = os.path.join(self.outdir_tsp_full, config["name"] + ".txt")
+        with open(outfile_path, "w") as outfile:
+            edge_count = config["node_count"] * (config["node_count"] - 1)
+            # header : node_count edge_count
+            outfile.write("%d %d" % (config["node_count"], edge_count))
+            # contents : start_node end_node weight
+            for idx, vertex in enumerate(graph.iter("vertex")):
+                for edge in vertex.iter("edge"):
+                    outfile.write("\n%d %d %d" %
+                                  (idx, int(edge.text), int(float(edge.attrib["cost"]))))
+
+
+def sample():
+    """ sample for TSPFetcher """
+    fetcher = TSPFetcher()
+    configs = fetcher.list()
+    fetcher.fetch(configs[0])
+    #fecther.fetch_all()
+
+
+if __name__ == "__main__":
+    sample()
