189dc6dc5a14b2b7f4ef43878bbcbd92417295f2
Yujin Jeong <jjiny1127@gmail.com>
2016-10-09 20:07:08 +0900

    euler algo

---
+++ Euler/Graph.h
@@ -0,0 +1,31 @@
+#ifndef EULER_GRAPH_H
+#define EULER_GRAPH_H
+
+#include <iostream>
+#include <list>
+#include <fstream> //file I/O library
+
+using namespace std;
+
+class Graph {
+private:
+    int V;
+    list<int> *adj;
+public:
+    Graph(int V);
+    void addEdge(int u, int v);
+    void rmvEdge(int u, int v);
+
+    // Methods to print Eulerian tour
+    void printEulerTour(ofstream &outstream);
+    void printEulerUtil(int s, ofstream &outstream);
+
+    // This function returns count of vertices reachable from v. It does DFS
+    int DFSCount(int v, bool visited[]);
+
+    // Utility function to check if edge u-v is a valid next edge in
+    // Eulerian trail or circuit
+    bool isValidNextEdge(int u, int v);
+};
+
+#endif
